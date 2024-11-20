5d5f18c13e50fbb253b7be7535276fafd0793a86
Alexey Muhamatnabeev <muhamatnabeev@yandex.ru>
2024-11-20 19:08:27 +0300

    add io delay in constraints

--- original_clocks.tcl
+++ original_clocks.tcl
@@ -1,3 +1,7 @@
 
 create_clock -name original_clock -period 5.000 [get_ports clock]
 
+set_input_delay 0.5 -clock [get_clocks original_clock] [get_ports in]
+set_input_delay 0.5 -clock [get_clocks original_clock] [get_ports rst]
+set_output_delay 0.5 -clock [get_clocks original_clock] [get_ports out]
+
